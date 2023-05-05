2ea5b48731f4b27dd590be25fe42f2c455375bb4
renoris <celbever@naver.com>
2023-05-05 19:58:00 +0900

    [problem] 문제 올림

--- src/KH/_5_8/Main2.java
+++ src/KH/_5_8/Main2.java
@@ -2,7 +2,7 @@ package KH._5_8;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String[] array;
