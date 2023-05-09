f1d836c6cb23fb34e9dc99597ba53632b1c49f45
jason191188 <mytreasure1164@gmail.com>
2023-05-10 01:56:48 +0900

    0510과제

---
+++ src/KH/_5_10/GunType.java
@@ -0,0 +1,17 @@
+package KH._5_10;
+
+public class GunType extends Weapon{
+    public GunType(String name, int damage) {
+        super(name, damage);
+    }
+
+//    public GunType(){
+//        super("", 0);
+//    }
+
+
+    @Override
+    protected String getWeaponUseMessage() {
+        return name+"으로 총알을 발사하여";
+    }
+}
