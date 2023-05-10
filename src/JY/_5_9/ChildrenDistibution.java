c865c0977c85cde5525e6005ae0a179667f345e8
yong <codoza@hanmail.net>
2023-05-10 16:08:31 +0900

    5월10일 문제풀이

--- src/JY/_5_9/ChildrenDistibution.java
+++ src/JY/_5_9/ChildrenDistibution.java
@@ -1,29 +1,39 @@
 package JY._5_9;
 
 
+
+
 public class ChildrenDistibution extends Member {
     String children;
+
+    public ChildrenDistibution() {
+        super("", 0);
+    }
 
     public ChildrenDistibution(String name, int age, String children) {
         super(name, age);
         this.children = children;
     }
 
-    public void convertMember() {
-        if (getAge() >= 19) {
-            children = "Y";
-        } else {
-            children = "N";
+    public Member[] convertMember(Member[] members) {
+        Member[] members2 = new ChildrenDistibution[members.length];
+        for (int i = 0; i < members.length; i++) {
+            if (members[i].getAge() <= 19) {
+                children = "Y";
+            } else {
+                children = "N";
+            }
+            members2[i] = new ChildrenDistibution(members[i].getName(), members[i].getAge(), children);
         }
+        return members2;
     }
-
-
-
 
     @Override
     public String toString() {
-        return String.format("이름: %-9s 나이:%-3d 미성년자 유무:%s", getName(),getAge(),children);
+        return super.toString()+"미성년자 유무 : "+children;
     }
 }
+
+
 
 
