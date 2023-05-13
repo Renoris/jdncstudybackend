09587c5d5a67667cfaea81328477927183d537f9
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2023-05-13 13:14:03 +0900

    세환_0515_월
    제출

---
+++ src/sehwan/_5_15/beakjoon10798.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class beakjoon10798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb  = new StringBuilder();
+        String[][] testCases = new String[5][];
+
+        //5개의 문자열을 입력받습니다.
+        for(int i = 0; i < 5; i++) {
+            testCases[i] = br.readLine().split("");
+        }
+
+        //문자열의 최대 길이(15)만큼 반복합니다.
+        //문자열마다 순차적으로 글자를 추가합니다. 문자열이 끝난 경우에는 예외가 떠서 무시됩니다.
+        for(int i = 0; i < 15; i++) {
+            for(int j = 0; j < 5; j++) {
+                try {
+                    sb.append(testCases[j][i]);
+                }
+                catch (Exception e) {
+
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
