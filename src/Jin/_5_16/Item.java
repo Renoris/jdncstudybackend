a42e866de5d762b43a28dd30b300c8cb35af7898
Jintime <wlstjd7391@gmail.com>
2023-05-16 02:27:00 +0900

    몰루

---
+++ src/Jin/_5_16/Item.java
@@ -0,0 +1,10 @@
+package Jin._5_16;
+
+    public class Item {
+
+        protected String name;
+
+        public Item(String name) {
+            this.name = name;
+        }
+    }
---
+++ src/Jin/_5_16/Potion.java
@@ -0,0 +1,5 @@
+package Jin._5_16;
+
+public interface Potion {
+    public void use(Member member);
+}
