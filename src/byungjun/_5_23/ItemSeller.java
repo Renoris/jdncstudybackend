cb03037300719311769ef4ff9eeb9321943d7ca3
renoris <celbever@naver.com>
2023-05-23 09:23:19 +0900

    [byungjun_5_23] 5/23 문제 풀이

---
+++ src/byungjun/_5_23/ItemSeller.java
@@ -0,0 +1,20 @@
+package byungjun._5_23;
+
+import java.util.Map;
+
+public class ItemSeller {
+
+    public static void sell(Member member, String itemList) {
+        String[] splitItemList = itemList.split(",");
+        Map<String, Integer> map = ItemList.getItemPriceMap();
+        for (String itemName : splitItemList) {
+            if (map.containsKey(itemName)) {
+                int price = map.get(itemName);
+                if (member.payMoney(price, itemName)) member.addItem(new Item(itemName, price));
+            }
+
+        }
+
+    }
+
+}
