b0a33aa89e62aae0c0080bcfcaf3ea66bdbf9a0b
renoris <celbever@naver.com>
2023-05-31 01:20:15 +0900

    [byungjun_5_31] 5/31 알고리즘 문제풀이

---
+++ src/byungjun/_5_31/secretmap/Solution.java
@@ -0,0 +1,18 @@
+package byungjun._5_31.secretmap;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = parseMap((long)arr1[i] | (long) arr2[i],n);
+        }
+
+        return answer;
+    }
+
+    private String parseMap(long num, int n) {
+        String format = "%0"+n+"d";
+        return String.format(format, Long.parseLong(Long.toBinaryString(num))).replace("0"," ").replace("1","#");
+    }
+}
