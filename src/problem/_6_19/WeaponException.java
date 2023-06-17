7b51ef0c00abe46b27654c1d3d74eeb007386541
renoris <celbever@naver.com>
2023-06-17 23:59:26 +0900

    [problem_6_19] 6/19-21 문제 출제

---
+++ src/problem/_6_19/WeaponException.java
@@ -0,0 +1,11 @@
+package problem._6_19;
+
+public abstract class WeaponException extends RuntimeException {
+
+    public WeaponException() {
+    }
+
+    public WeaponException(String message) {
+        super(message);
+    }
+}
