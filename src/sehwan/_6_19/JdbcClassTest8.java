3aea3a86c984ae973ea8ee4bbb0e3c2bc118d35d
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2023-06-21 13:19:20 +0900

    s0621
    d

---
+++ src/sehwan/_6_19/JdbcClassTest8.java
@@ -0,0 +1,26 @@
+package sehwan._6_19;
+
+public class JdbcClassTest8 {
+
+    public static void main (String[] args) {
+        WeaponProvider weaponProvider = new WeaponProvider();
+        Member[] members = getMemberList();
+
+        for(Member member : members) {
+            try {
+                Member equipWeaponMember = weaponProvider.getEquipWeaponMember(member);
+                System.out.println(equipWeaponMember.getProfile());
+            } catch (WeaponException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static Member[] getMemberList() {
+        return new Member[] {
+            new Member("Danu", RangeType.SHORT),
+            new Member("Nuada", RangeType.SHORT),
+            new Member("Morríghan", RangeType.LONG)
+        };
+    }
+}
---
+++ src/sehwan/_6_19/NullWeaponException.java
@@ -0,0 +1,7 @@
+package sehwan._6_19;
+
+public class NullWeaponException extends WeaponException {
+    public NullWeaponException(String message) {
+        super(message);
+    }
+}
